3b2f514844888edf442d5c4c6ffa46225e851a84
Alireza Nejati <121515026+Alireza-njt@users.noreply.github.com>
2025-07-22 08:27:17 +0330

    Update shirtificate.py

--- week8/CS50 Shirtificate/shirtificate.py
+++ week8/CS50 Shirtificate/shirtificate.py
@@ -1,1 +1,16 @@
 # Alireza Nejati (alirezanejatiz27@gmail.com)
+# Tuesday , July 22 , 2025
+
+from fpdf import FPDF
+
+name = input("Name: ").strip()
+pdf = FPDF()
+pdf.add_page()
+pdf.set_auto_page_break(auto=False, margin=0)
+pdf.set_font("Helvetica", "B", 50)
+pdf.cell(0, 50, txt="CS50 Shirtificate", align="C", new_x="LMARGIN", new_y="NEXT")
+pdf.image("shirtificate.png", x=5, y=80, w=200)
+pdf.set_font("Helvetica", "B", size=30)
+pdf.set_text_color(255, 255, 255)
+pdf.cell(0, 180, align="C", txt=f"{name} took CS50")
+pdf.output("shirtificate.pdf")
